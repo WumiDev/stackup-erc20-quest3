8a4ea902655fd374aa2a19f1cd302910011d7eb1
WumiDev <wumi2021@gmail.com>
2023-07-18 05:24:28 +0000

    implement erc20 extensions

--- contracts/StackupVault.sol
+++ contracts/StackupVault.sol
@@ -11,16 +11,28 @@ contract StackupVault {
 
     constructor(address uniAddr, address linkAddr) {
         // initialize mapping of underlying token address => claim tokens
+        claimTokens[uniAddr] = new sToken("Claim Uni", "sUNI");
+        claimTokens[linkAddr] = new sToken("Claim Link", "sLINK");
+        
         // initialize mapping of underlying token address => underlying tokens
+        tokens[uniAddr] = IERC20(uniAddr);
+        tokens[linkAddr] = IERC20(linkAddr);
     }
 
     function deposit(address tokenAddr, uint256 amount) external {
         // transfer underlying tokens from user to vault, assume that user has already approved vault to transfer underlying tokens
+        require(tokens[tokenAddr].transferFrom(msg.sender, address(this), amount), "transferFrom failed");
+        
         // mint sTokens
+        claimTokens[tokenAddr].mint(msg.sender, amount);
     }
 
     function withdraw(address tokenAddr, uint256 amount) external {
         // burn sTokens
+        claimTokens[tokenAddr].burn(msg.sender, amount);
+        
         // transfer underlying tokens from vault to user
+        require(tokens[tokenAddr].transfer(msg.sender, amount), "transfer failed");
     }
 }
+
--- contracts/sToken.sol
+++ contracts/sToken.sol
@@ -5,9 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-// add inheritance
-contract sToken {
+    // add inheritance
+    contract sToken is ERC20, Ownable, ERC20Burnable {
     // insert constructor function here
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    }
+
     // insert mint function here
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
     // insert burn function here
+    function burn(address to, uint256 amount) external onlyOwner {
+        _burn(to, amount);
+    }
+
 }
+
